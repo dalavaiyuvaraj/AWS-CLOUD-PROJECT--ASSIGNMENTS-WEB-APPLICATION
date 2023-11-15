fba5052265b395f6c0f6e02fbdb4d864bcf62967
dalavaiyuvaraj <dalavai.y@northeastern.edu>
2023-11-15 15:48:06 -0500

    separting cloudwatch config as file provision from install script

--- install.sh
+++ install.sh
@@ -23,50 +23,14 @@ sudo curl -o amazon-cloudwatch-agent.deb https://amazoncloudwatch-agent.s3.amazo
 
 sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
 
-sudo touch cloudwatch-config.json
-
-sudo chmod 777 cloudwatch-config.json
-
-sudo cat <<EOF > cloudwatch-config.json
-{
-    "agent": {
-        "metrics_collection_interval": 10,
-        "logfile": "/var/logs/amazon-cloudwatch-agent.log"
-    },
-    "logs": {
-        "logs_collected": {
-            "files": {
-                "collect_list": [
-                    {
-                        "file_path": "/opt/csye6225/webapp/logs/csye6225.log",
-                        "log_group_name": "csye6225",
-                        "log_stream_name": "webapp"
-                    }
-                ]
-            }
-        },
-        "log_stream_name": "cloudwatch_log_stream"
-    },
-    "metrics":{
-      "metrics_collected":{
-         "statsd":{
-            "service_address":":8125",
-            "metrics_collection_interval":10,
-            "metrics_aggregation_interval":15
-            }
-        }
-    }
-}
-EOF
-
-sudo mv cloudwatch-config.json /opt/aws/amazon-cloudwatch-agent/bin/
-
 sudo groupadd csye6225
 sudo useradd -s /bin/false -g csye6225 -d /opt/csye6225 -m csye6225
 
 sudo mkdir "/opt/csye6225/webapp"
 # Unzip the file to the destination directory
 sudo unzip "$ZIP_FILE" -d "$DEST_DIR"
+
+sudo mv "/opt/csye6225/webapp/cloudwatch-config.json" "/opt/aws/amazon-cloudwatch-agent/bin/"
 
 cd "/opt/csye6225/webapp"
 
